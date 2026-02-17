fc5715d4d7a9710b5a4fce895d2c85b053c635c3
Gitm3h <elmekdad.mahmoud.1@gmail.com>
2026-02-17 22:27:08 +0100

    Update fridge.py
    Invited the player to take a look in the fridge and interact with the game.
    
    Assuming the player was hungry and wanted to make a sandwitch, they would need their favorite ingredients

--- fridge.py
+++ fridge.py
@@ -1,11 +1,14 @@
 # Not used at the moment, but there for the test example
-def evaluate(contents, food):
+def evaluate(contents, food): #homework 2 hint:  def evaluate(contents, first, second):  then replace the body of the function
     if contents == food:
         print("Yes, it's all food!!!")
         return True
     else:
         print("That's not all of the food, or there is still something you shouldn't eat!!!")
         return False
+
+def combine_foods(a, b):
+    return a + b
 
 def fridge():
     print("Welcome to the fridge")
@@ -16,9 +19,23 @@ def fridge():
     print("You have in your fridge:\n")
     for x in contents:
         print(x)
+    print("You still need:\n")
+    for x in shopping_list:
+        print(x)
+    a = str(input("Enter an item: "))
+    b = str(input("Enter another item: "))
+    if a in contents and b in contents:
+      print(f"The sum of {a} and {b} is {combine_foods(a, b.lower())}")
+    elif a not in contents and b in contents:
+      print(f"You don't have {a}, but you do have {b}" )
+    if a in contents and b not in contents:
+        print(f"You do not have {b}, which would go well with {a}")  
+    else:
+      print(f"You do not have either {a} nor {b}. Go shopping?")  
 
+#'homework 1': The printing of the lists repeats. Can you turn that into a function that both can use?
+#'homework 2': See 'evaluate()' above? Can you replace that with what you are evaluating in fridge()?
+#'homework 3': Can you update the fridge_test.py to test the new evaluate()?
 
 if __name__ == "__main__":
     fridge()
-
-
