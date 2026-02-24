4ac3198a0a0e80145db5a5998d42b99ccd225fa3
Gitm3h <elmekdad.mahmoud.1@gmail.com>
2026-02-24 14:24:42 +0100

    Update fridge.py
    Updated evaluate function

--- fridge.py
+++ fridge.py
@@ -1,41 +1,42 @@
 # Not used at the moment, but there for the test example
-def evaluate(contents, food): #homework 2 hint:  def evaluate(contents, first, second):  then replace the body of the function
-    if contents == food:
-        print("Yes, it's all food!!!")
+def evaluate(contents, shopping_list, combine_foods): #homework 2 hint:  def evaluate(contents, first, second):  then replace the body of the function
+    if contents == True:
+        print("Guten Apetit!!!")
         return True
     else:
-        print("That's not all of the food, or there is still something you shouldn't eat!!!")
+        print("There's nothing to eat!!!")
         return False
+
+def loop(x):
+    for x in x:
+        print(x)
 
 def combine_foods(a, b):
     return a + b
 
 def fridge():
     print("Welcome to the fridge")
+    print("You currently have:\n")
 
     # "emmental" could also be a cheese, in the future
-    contents = ["Cheese", "Butter", "Peas", "Milk"]
-    shopping_list = ["Cheese", "Butter", "Milk", "Eggs", "Salami", "Lettuce"]
-    print("You have in your fridge:\n")
-    def loop(x):
-        for x in x:
-            print(x)
+    contents = ["Emmental", "Butter", "Peas", "Milk", "Bread"]
     loop(contents)
-    print("You still need:\n")
+    print ("You still need:\n")
+    shopping_list = ["Cheddar", "Mushrooms", "Honey", "Eggs", "Salami", "Lettuce"]
     loop(shopping_list)
     a = str(input("Enter an item: "))
     b = str(input("Enter another item: "))
-    if a in contents and b in contents:
-      print(f"The sum of {a} and {b} is {combine_foods(a, b.lower())}")
-    elif a not in contents and b in contents:
+    if a and b in contents:
+        print(f"The sum of {a} and {b} is {combine_foods(a, b.lower())}")
+    elif a not in contents:
       print(f"You don't have {a}, but you do have {b}" )
-    if a in contents and b not in contents:
+    if b not in contents:
         print(f"You do not have {b}, which would go well with {a}")  
     else:
-      print(f"You do not have either {a} nor {b}. Go shopping?")  
+        print(f"You do not have either {a} nor {b}. Go shopping?")  
 
-#'homework 1': The printing of the lists repeats. Can you turn that into a function that both can use?
-#'homework 2': See 'evaluate()' above? Can you replace that with what you are evaluating in fridge()?
+#'homework 1': The printing of the lists repeats. Can you turn that into a function that both can use? Done
+#'homework 2': See 'evaluate()' above? Can you replace that with what you are evaluating in fridge()? Maybe done?
 #'homework 3': Can you update the fridge_test.py to test the new evaluate()?
 
 if __name__ == "__main__":
