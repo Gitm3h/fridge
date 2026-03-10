4258d930f4088bbe5fbabc033bc05f7260bc542a
Gitm3h <elmekdad.mahmoud.1@gmail.com>
2026-03-10 22:46:18 +0100

    Update fridge-test.py
    Added a test for each condition in the main code. Fixed some bugs, and added a test for a future condition

--- fridge-test.py
+++ fridge-test.py
@@ -1,21 +1,39 @@
 import unittest
 from fridge import evaluate
 
-# This fails, WTF?
 
 class FridgeTests(unittest.TestCase):
-    def test_evaluate(self):
-        result = evaluate(["a","b"], ["a", "b"])
-        self.assertTrue(result)
+    contents = ["A","B","C"]
+
+    def test_evaluate_both_found(self):
+      first = "B"
+      result = evaluate(self.contents, first, "C")
+      self.assertEqual(result, 2)
+
+    def test_evaluate_both_both_but_are_lowercase(self):
+      result = evaluate(self.contents, "a", "b")
+      self.assertEqual(result, 2)
+
+    def test_evaluate_neither_found(self):
+      result = evaluate(self.contents, "???", "?!")
+      self.assertEqual(result, 0)
+
+    def test_evaluate_found_notfound(self):
+      result = evaluate(self.contents, "A", "X")
+      self.assertEqual(result, 1)
+
+    def test_evaluate_notfound_found(self):
+      result = evaluate(self.contents, "X", "B")
+      self.assertEqual(result, 1)
+    
+    def test_evaluate_both_found_but_both_same(self):
+      result = evaluate(self.contents, "a", "a")
+      self.assertEqual(result, 1)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
